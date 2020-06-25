7c0125a85e93d261ffa08db2abdab843ea1d86f5
KhizhniakovM <KhizhniakovMA@gmail.com>
2020-06-25 14:36:08 +0300

    Initial commit

---
+++ WhereToBuyWine/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  WhereToBuyWine
+//
+//  Created by Max Khizhniakov on 25.06.2020.
+//  Copyright © 2020 Max Khizhniakov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = RootViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
