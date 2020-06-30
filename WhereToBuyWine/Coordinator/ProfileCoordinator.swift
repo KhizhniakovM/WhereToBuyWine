8e2ceb80c814c4376ea8f6b0858e010edd897227
KhizhniakovM <KhizhniakovMA@gmail.com>
2020-06-30 14:56:12 +0300

    Add Shops, Wine

--- WhereToBuyWine/Coordinator/ProfileCoordinator.swift
+++ WhereToBuyWine/Coordinator/ProfileCoordinator.swift
@@ -2,7 +2,7 @@
 //  ProfileCoordinator.swift
 //  WhereToBuyWine
 //
-//  Created by Max Khizhniakov on 26.06.2020.
+//  Created by Max Khizhniakov on 30.06.2020.
 //  Copyright © 2020 Max Khizhniakov. All rights reserved.
 //
 
